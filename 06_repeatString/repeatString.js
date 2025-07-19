f719cbc7e7f9b948280d8c1795722c7cfa0383af
fzd <fzddnp@hotmail.com>
2025-07-19 21:22:57 +0330

    Complete exercises 6 to 11

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let result = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    while (num) {
+      result += str;
+      num--;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  let sum = 0;
+  if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+    return "ERROR";
+  } else {
+    let min = Math.max(x, y);
+    let max = Math.min(x, y);
 
+    for (let i = max; i <= min; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
